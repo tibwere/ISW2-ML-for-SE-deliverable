8a558c1eda943545792db1b00dcf25135ab7fc1c
Simone Tiberi <tib.sim.98@gmail.com>
2021-11-15 23:43:00 +0100

    #1

---
+++ src/main/java/it/uniroma2/isw2/deliverable2/App.java
@@ -0,0 +1,13 @@
+package it.uniroma2.isw2.deliverable2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
