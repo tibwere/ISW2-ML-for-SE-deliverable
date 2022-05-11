106cc37112a842778dcf125492e4e30e6baaa25f
Simone Tiberi <tib.sim.98@gmail.com>
2022-05-11 19:27:34 +0200

    #38

--- src/main/java/it/uniroma2/isw2/deliverable2/exceptions/MaximumRequestToGithubAPIException.java
+++ src/main/java/it/uniroma2/isw2/deliverable2/exceptions/MaximumRequestToGithubAPIException.java
@@ -2,6 +2,8 @@ package it.uniroma2.isw2.deliverable2.exceptions;
 
 public class MaximumRequestToGithubAPIException extends Exception {
     public MaximumRequestToGithubAPIException() {
-        super("The maximum Github REST API request limit has been reached");
+        super("The maximum Github REST API request limit has been reached" +
+                "For more information please visit:\n" +
+                "https://docs.github.com/en/developers/apps/building-github-apps/rate-limits-for-github-apps#default-user-to-server-rate-limits-for-githubcom ");
     }
 }
